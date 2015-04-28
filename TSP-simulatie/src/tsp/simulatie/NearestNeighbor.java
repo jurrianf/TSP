7c896e468a1d65a0bd1c9717444f502993ec815c
jurrianf <jurrian_fischer@hotmail.com>
2015-04-28 14:29:06 +0200

    begin NearestNeighbor

---
+++ TSP-simulatie/src/tsp/simulatie/NearestNeighbor.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tsp.simulatie;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Jurrian
+ */
+public class NearestNeighbor {
+
+    private ArrayList<Vak> geselecteerd = new ArrayList<>();
+    private ArrayList<Vak> route = new ArrayList<>();
+
+}
