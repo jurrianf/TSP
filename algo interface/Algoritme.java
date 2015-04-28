df0a2144d7afef1407278823a1d506cb867cad56
PinkishYellow <vincentkloppenburg1996@live.nl>
2015-04-28 14:35:01 +0200

    Algoritme
    het interface voor algo Alst U Blieft

---
+++ algo interface/Algoritme.java
@@ -0,0 +1,23 @@
+package tsp.simulatie;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Johan
+ */
+public interface Algoritme {
+
+    
+    void Algoritme(Order order);
+    
+    Order getOrder();
+    
+    Route getRoute();
+    
+    Route berekenRoute(Order order);
+}
