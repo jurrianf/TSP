862e8fface9697910b91087a90c108b8c62ed303
jurrianf <jurrian_fischer@hotmail.com>
2015-05-11 16:40:41 +0200

    neighbor

--- TSP-simulatie/src/tsp/simulatie/NearestNeighbor.java
+++ TSP-simulatie/src/tsp/simulatie/NearestNeighbor.java
@@ -5,6 +5,7 @@
  */
 package tsp.simulatie;
 
+import java.awt.Graphics;
 import java.util.ArrayList;
 
 /**
@@ -13,55 +14,12 @@ import java.util.ArrayList;
  */
 public class NearestNeighbor extends MyMath implements Algoritme {
 
-    /*   private ArrayList<Vak> alleVakken = new ArrayList<Vak>();
-     private ArrayList<Vak> lijst = new ArrayList<>();
-     private ArrayList<Vak> route = new ArrayList<>();
-
-     public ArrayList<Vak> setlijst(Vak a) {
-
-     for (int i = 0; i < alleVakken.size(); i++) {
-     for (Vak check : alleVakken) {
-
-     if (check.getIsGeselecteerd()) {
-     lijst.add(alleVakken.get(i));
-     }
-     }
-     }
-     System.out.println(lijst);
-     return lijst;
-     }
-
-     public ArrayList<Vak> getlijst(Vak a) {
-     return lijst;
-     }
-
-     public ArrayList<Vak> setroute() {
-     int x = 0;
-     int y = 0;
-     while (lijst.size() >= 1) {
-     for (int i = 0; i < lijst.size(); i++) {
-     delta berekening
-     int afstand = 0;
-     if ((afstand > delta)  {
-     OR(afstand == 0)
-     }
-
-     )
-     {
-     afstand = delta;
-     return delta;
-     }
-     route.add(lijst.get(i));
-     lijst.remove(i);
-     }
-     }
-     return route;
-     } */
     Order order;
     Route route;
     Vak routeVakken[][];
     Vak vak;
     ArrayList<Vak> vakken;
+    ArrayList<Vak> volgorde = new ArrayList<>();
 
     @Override
     public void Algoritme(Order order) {
@@ -91,9 +49,11 @@ public class NearestNeighbor extends MyMath implements Algoritme {
         vak = vakken.get(0);
         while (1 < vakken.size()) {
             calcLoop(0, vakken, vak);
-            System.out.println(vak);
+            //System.out.println(vak);
 
         }
+        System.out.print(volgorde);
+        System.out.print(vakken);
 
     }
 
@@ -117,13 +77,13 @@ public class NearestNeighbor extends MyMath implements Algoritme {
                     delta = tempDelta;
                     vak = v.get(i);
                     index = i;
-                    System.out.println(vak.toString() + "   delta: " + delta + " |i = " + i + " ,j = " + j);
+                    //System.out.println(vak.toString() + "   delta: " + delta + " |i = " + i + " ,j = " + j);
                 }
 
                 for (int a = 0; a < vakken.size(); a++) {
                     if (vakken.get(a).equals(curVak)) {
+                        volgorde.add(curVak);
                         vakken.remove(vakken.get(a));
-                        System.out.println("desu kawaii");
                     }
                 }
 
